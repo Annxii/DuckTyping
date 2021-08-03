764ee0806b0032b84e50ada152530d9dd948583f
Torsten Holtse <torsten.holtse@LIVE.DK>
2021-08-03 20:39:56 +0200

    cleanup'n'refactor

--- DuckTyping/Program.cs
+++ DuckTyping/Program.cs
@@ -7,67 +7,15 @@ using System.Threading.Tasks;
 
 namespace DuckTyping
 {
-
-    public interface IService
-    {
-        string Echo(string input);
-    }
-
-    public class ServiceImpl : IService
-    {
-        public string Echo(string input)
-        {
-            return $"Echo: {input}";
-        }
-    }
-
-    public class DummyImpl : IService
-    {
-        private readonly IService instance;
-
-        public DummyImpl(IService x)
-        {
-            this.instance = x;
-        }
-
-        public string Echo(string input)
-        {
-            Console.WriteLine("Begin Echo");
-            var stamp = DateTime.Now;
-            var result = instance.Echo(input);
-            var x = DateTime.Now.Subtract(stamp);
-            Console.WriteLine(x.ToString());
-            Console.WriteLine(result);
-            Console.WriteLine("End Echo");
-            return result;
-        }
-
-        public void ArrayTest()
-        {
-            var arr = new int[1];
-            arr[0] = 1;
-
-            Console.WriteLine(arr.Length);
-            Console.WriteLine(arr[0]);
-        }
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            ExecuteDuckTyping().Wait();
-
-            //ExecuteProxy();
+            ExecuteDuckEnumeration();
+            await ExecuteDuckTask();
         }
 
-        private static void ExecuteProxy()
-        {
-            var service = CustomProxy<IService>.Wrap(new ServiceImpl());
-            var result = service.Echo("hello");
-        }
-
-        static async Task ExecuteDuckTyping()
+        static void ExecuteDuckEnumeration()
         {
             // DuckEnumeration
             var enumeration = new DuckEnumeration();
@@ -76,7 +24,10 @@ namespace DuckTyping
                 Console.WriteLine(item);
             }
             Console.WriteLine();
+        }
 
+        static async Task ExecuteDuckTask()
+        {
             // DuckTask
             var task = new DuckTask("async duck");
             var asyncItem = await task;
